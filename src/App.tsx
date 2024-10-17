5af382aefbfd265594d31dfa8a5c1b7af887bde9
facundo pedrozo <facundopedrozo2@gmail.com>
2024-10-16 21:37:40 -0300

    probando

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LogIn from './components/Login'
 import DashboardAdmin from './components/DashboardAdmin';
 import ClientManagement from './components/ClientManagAdmin';
+import TraineersManager from './components/TraineersManager';
 
 function App() {
   return (
@@ -12,6 +13,10 @@ function App() {
       <Route path="/" element={<LogIn/>} />
       <Route path="/admin" element={<DashboardAdmin/>} />
       <Route path="/admin/clients" element={<ClientManagement/>} />
+      <Route path="/clients" element={<ClientManagement/>} />
+
+      <Route path="/traineers" element={<TraineersManager/>} />
+
     </Routes>
   </Router>
   )
